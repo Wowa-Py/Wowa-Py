5c8d97f172a7dd384ac551573ae56a687c17dc3a
Peshkov <135055914+Wowa-Py@users.noreply.github.com>
2024-03-20 17:18:20 +0300

    Create Chart.js

---
+++ Chart.js
@@ -0,0 +1,23 @@
+<canvas id="myChart" width="400" height="400"></canvas>
+<script src="path/to/Chart.js"></script>
+<script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'red',
+                'blue',
+                'yellow',
+                'green',
+                'purple',
+                'orange'
+            ]
+        }]
+    }
+});
+</script>
